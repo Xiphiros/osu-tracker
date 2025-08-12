6e9114e6f8f149526a7ac58a191e5e841bd41763
Xiphiros <72585136+Xiphiros@users.noreply.github.com>
2025-08-12 11:27:52 -0400

    refactor: use relative path for api calls

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://127.0.0.1:5000/api';
+const API_BASE_URL = '/api'; // Use a relative path
 
 export const getReplays = async () => {
     const response = await fetch(`${API_BASE_URL}/replays`);
